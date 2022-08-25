527788766a6be052f41674d0ee3503ea21436948
vivian <vivian.v18723@gmail.com>
2022-08-25 12:28:17 +0100

    create & export App function. import & use useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,28 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
+import "../../assets/search.svg";
 import "./App.css";
-import { Person } from "../Main/index";
+import moviesUrl from "../../env.js";
+
+const API_URL = moviesUrl;
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
 
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("clifford");
+  }, []);
+
+  return (
+    <>
+      <h1>Hi there 👋</h1>
+    </>
+  );
 };
 
 export default App;
