b71306aa1bf3ad9b7eb4455e0b279255fe739756
vivian <vivian.v18723@gmail.com>
2022-08-26 16:20:28 +0100

    import & use NavLink & Outlet

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 import "./navbar.css";
 
 const NavBar = () => {
+  const activeLink = {color: "#f0f0f0"};
+
+  function toggleActive({isActive}) {
+    if (isActive) {
+      return activeLink;
+    } else{
+      return undefined;
+    }
+  }
+
   return (
     <>
       <nav className="nav_bar">
         <ul>
-          <li>Home</li>
-          <li>Buy Tickets</li>
-          <li>About</li>
+          <li>
+            <NavLink to={"/"} style={toggleActive}>Home |</NavLink>
+          </li>
+          <li>
+            <NavLink to={"buy_tickets"} style={toggleActive}>| Buy Tickets |</NavLink>
+          </li>
+          <li>
+            <NavLink to={"about"} style={toggleActive}>| About</NavLink>
+          </li>
         </ul>
       </nav>
+      <Outlet />
     </>
   );
 };
