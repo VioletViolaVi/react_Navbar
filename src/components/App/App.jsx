83547291e0f177c627d860d9657b0a83e041392a
vivian <vivian.v18723@gmail.com>
2022-08-26 12:51:22 +0100

    move code within <main></main>... ...to own HomePage component move useState & useEffect to HomePage component move its respective imports to HomePage component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,85 +1,20 @@
-import { useEffect, useState } from "react";
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-import SearchIcon from "../../assets/search.svg";
-import moviesUrl from "../../env.js";
-import MoviesCard from "../Main/MoviesCard/index";
-import About from "../Main/About";
-import BuyTickets from "../Main/BuyTickets";
+import AboutPage from "../AboutPage";
+import BuyTicketsPage from "../BuyTicketsPage";
 import NavBar from "../Header/NavBar";
-
-const API_URL = moviesUrl;
+import HomePage from "../HomePage";
 
 const App = () => {
-  const [allMovies, setAllMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // its own function
-  const searchMovies = async (title) => {
-    const res = await fetch(`${API_URL}&s=${title}`);
-    const data = await res.json();
-
-    setAllMovies(data.Search);
-  };
-
-  useEffect(() => {
-    searchMovies("clifford");
-  }, []);
-
   return (
     <>
       <NavBar />
-      <main className="app">
-        <>
-          <h1>MovieLand</h1>
-          <section className="search">
-            <input
-              type="text"
-              placeholder="Search for movies"
-              value={searchTerm}
-              onChange={(e) => {
-                return setSearchTerm(e.target.value);
-              }}
-            />
-            <img
-              src={SearchIcon}
-              alt="search"
-              onClick={() => {
-                return searchMovies(searchTerm);
-              }}
-            />
-          </section>
-
-          {allMovies.length > 0 ? (
-            <section className="container">
-              {allMovies.map((singleObj) => {
-                return (
-                  <MoviesCard
-                    // wholeObj={singleObj} // a shorter/faster way than below ;)
-                    key={singleObj.imdbID}
-                    Year={singleObj.Year}
-                    imdbID={singleObj.imdbID}
-                    Poster={singleObj.Poster}
-                    Title={singleObj.Title}
-                    Type={singleObj.Type}
-                  />
-                );
-              })}
-            </section>
-          ) : (
-            <div className="empty">
-              <h2>No movies found</h2>
-            </div>
-          )}
-        </>
-        <>
-          <About />
-        </>
-        <>
-          <BuyTickets />
-        </>
-      </main>
+      <HomePage />
+      <AboutPage />
+      <BuyTicketsPage />
     </>
   );
 };
