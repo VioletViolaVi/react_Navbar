ebf5de47d1b81b2889a88e5abe1cf5f1636d415e
vivian <vivian.v18723@gmail.com>
2022-08-26 14:49:23 +0100

    import & use NavBar & Footer components... ...within BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App/App";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <NavBar />
       <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
