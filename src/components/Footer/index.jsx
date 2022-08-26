058a8760f53514148c0c2dfd029a5c15acef9070
vivian <vivian.v18723@gmail.com>
2022-08-26 13:03:57 +0100

    create & export Footer component import css styles for Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer_bg">
+        <p>Copyright</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
